f86550df1d467dd72c8a594291b1f3f95b185528
tahiyaa734 <tahiatabassumsamiha@gmail.com>
2025-04-23 21:11:00 +0600

    add calculator.R file for lecture_2

---
+++ calculator.R
@@ -0,0 +1,27 @@
+5+3
+3+2
+3-2
+3*2
+3/2
+7%/%2 #integer division, only the quotient
+5%%3 #modulus division, the remainder
+(10-5)*(2+4) #use of parentheses
+10-5*2+4 
+(10-5)*(2+4) 
+7/(1+3); 7/1+3 
+1+2; log(1); 1/10
+a <- 5
+b <- 10
+a <- a + 10
+a^3
+a**2
+x <- 9/4
+floor(x)
+ceilling(x)
+round(x)
+round(x,2)
+a == b
+a > b #is a greater than b?
+a != b #is a not equal to b?
+a < b
+
