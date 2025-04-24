87b97756a58f6e0f2d899903448366d17fe8bd74
tahiyaa734 <tahiatabassumsamiha@gmail.com>
2025-04-24 23:57:26 +0600

    added new data files

---
+++ homework2/hw-1.R
@@ -0,0 +1,10 @@
+current_year <- 2025
+uni_start <- 2022
+birth_year <- 2003
+#difference between this year (2025) and university starting year=x
+#difference between this year and the year I were born=y
+x <- (current_year - uni_start)
+y <- (current_year - birth_year)
+percentage_at_uni <- (x/y)*100
+(x/y)*100
+#Percentage of years I spent in university is 13.63% of my total life.
---
+++ homework2/hw-2.R
@@ -0,0 +1,10 @@
+#numeric variables
+x <- 13.5
+#integer variables
+y <- 7
+#Numeric vector
+m <- c(2,4,6,8,10)
+#logical vector
+n <- c(TRUE,TRUE,FALSE,FALSE)
+#character vactor
+season <- c("summer","Rainy season","Winter","spring")
